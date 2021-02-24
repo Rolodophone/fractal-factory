c678e954d452c5b0a5b158e12fefc61572c3e604
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-02-24 20:21:14 +0000

    add drawing shape

--- src/main/kotlin/FractalFactory.kt
+++ src/main/kotlin/FractalFactory.kt
@@ -2,9 +2,12 @@ import org.openrndr.Program
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.math.Vector2
 
 lateinit var pg: Program
 var state = State.DRAW_SHAPE
+
+var shape = mutableListOf<MutableList<Vector2>>()
 
 enum class State {
     DRAW_SHAPE, DEFINE_NEXT_GEN, DISPLAY_FRACTAL
@@ -21,7 +24,23 @@ fun main() = application {
         extend {
             drawer.clear(ColorRGBa.GRAY)
 
+            //draw shape
+            drawer.fill = ColorRGBa.BLACK
+            drawer.strokeWeight = 10.0
+
+            for (lineStrip in shape) {
+                drawer.lineStrip(lineStrip)
+            }
+
             InfoText.draw()
+        }
+
+        mouse.buttonDown.listen {
+            shape.add(mutableListOf(it.position))
+        }
+
+        mouse.dragged.listen {
+            shape.last().add(it.position)
         }
     }
 }
